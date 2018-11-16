56387fc80b20ef39ee592c386bb8562b4a88aac5
董士程 <@LAPTOP-M4P27I4E>
2018-11-16 20:37:19 +0800

    修改普通Integer

--- STM/src/com/dsc/AtomicStampedReference/AtomicStamped.java
+++ STM/src/com/dsc/AtomicStampedReference/AtomicStamped.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class AtomicStamped {
 
-	//private static AtomicInteger atomicInt = new AtomicInteger(100);
+	private static AtomicInteger atomicInt = new AtomicInteger(100);
 	private static AtomicStampedReference atomicStampedRef = new AtomicStampedReference(100, 0);
 
 	public static void main(String[] args) throws InterruptedException {
@@ -48,29 +48,29 @@ public class AtomicStamped {
 		
 		
 		
-//		Thread intT1 = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				atomicInt.compareAndSet(100, 101);
-//				atomicInt.compareAndSet(101, 100);
-//			}
-//		});
-//
-//		Thread intT2 = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				try {
-//					TimeUnit.SECONDS.sleep(1);
-//				} catch (InterruptedException e) {
-//				}
-//				boolean c3 = atomicInt.compareAndSet(100, 101);
-////				System.out.println(c3);
-//			}
-//		});
-//
-//		intT1.start();
-//		intT2.start();
-//		intT1.join();
-//		intT2.join();
+		Thread intT1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				atomicInt.compareAndSet(100, 101);
+				atomicInt.compareAndSet(101, 100);
+			}
+		});
+
+		Thread intT2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+				}
+				boolean c3 = atomicInt.compareAndSet(100, 101);
+//				System.out.println(c3);
+			}
+		});
+
+		intT1.start();
+		intT2.start();
+		intT1.join();
+		intT2.join();
 	}
 }
