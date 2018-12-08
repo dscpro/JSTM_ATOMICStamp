91fc2d8c4cb1043e63823030029d811edd0dfd34
admin <admin@DESKTOP-565VLB9>
2018-12-08 10:06:55 +0800

    建立ATMIC类

--- STM/src/com/dsc/Transaction/Transaction.java
+++ STM/src/com/dsc/Transaction/Transaction.java
@@ -1,5 +1,69 @@
 package com.dsc.Transaction;
 
+ 
 public class Transaction {
+	protected static final ThreadLocal<Transaction> current = new ThreadLocal<Transaction>();
+	protected final Transaction parent;
+	protected int number;
+	public Transaction(int number) {
+        this(null, number);
+    }
+	public Transaction(Transaction parent, int number) {
+	        this.parent = parent;
+	        this.number = number;
+	}
+	// 获取当前事务
+	public static Transaction getCurrentTransaction() {
 
+		return current.get();
+	}
+
+	// 开启事务
+	public static Transaction startTransaction() {
+
+		Transaction tx = null;
+
+		tx = new UpdateTransaction(1);
+
+		tx.start();
+		return tx;
+	}
+
+	// 提交事务
+	public static void commitTransaction() {
+
+		Transaction tx = current.get();
+        tx.commitTx(true);
+	}
+	public void commitTx(boolean finishAlso) {
+        //doCommit();
+
+        if (finishAlso) {
+            finishTx();
+        }
+    }
+	private void finishTx() {
+        finish();
+
+        current.set(this.getParent());
+    }
+	 protected static void finish() {
+	        // intentionally empty
+	    }
+	// 关闭事务
+	public static void closeTransaction() {
+		
+	}
+
+	// 回滚
+	public static void rollBackTransaction() {
+		
+	}
+
+	public  void  start() {
+		current.set(this);
+	}
+	protected  Transaction getParent() {
+        return parent;
+    }
 }
--- STM/src/com/dsc/test/TraDemo.java
+++ STM/src/com/dsc/test/TraDemo.java
@@ -1,5 +1,56 @@
 package com.dsc.test;
 
-public class TraDemo {
+ 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
+import org.junit.Test;
+
+import com.dsc.Atomic.AtomicFactory;
+import com.dsc.Atomic.AtomicStamp;
+
+
+public class TraDemo {
+	static AtomicFactory asd= new AtomicStamp();
+	AtomicStampedReference atomicStampedRef;
+	
+	@Test
+	public  void test() {
+		
+		
+		//Transaction.startTransaction();
+		asd.setAtomicStamp(100);
+		atomicStampedRef= asd.getAtomicStamp();
+		
+		//System.out.println(c2+"重置标记"+atomicStamped.getStamp()+" 值:"+atomicStamped.getReference());
+		//Transaction.commitTransaction();
+		
+		Thread refT1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				
+				System.out.println("初始标记"+atomicStampedRef.getStamp()+" 值:"+atomicStampedRef.getReference());
+				boolean c1 =atomicStampedRef.compareAndSet(100, 101, atomicStampedRef.getStamp(), atomicStampedRef.getStamp() + 1);
+				System.out.println(c1+"变更一次标记"+atomicStampedRef.getStamp()+" 值:"+atomicStampedRef.getReference());
+				
+				
+			}
+		});
+
+		Thread refT2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				
+				System.out.println("当前标志: "+atomicStampedRef.getStamp()+" 值:"+atomicStampedRef.getReference());
+				boolean c3 = atomicStampedRef.compareAndSet(100, 101, atomicStampedRef.getStamp(), atomicStampedRef.getStamp()+ 1);
+				
+				System.out.println(c3+"当前标志"+atomicStampedRef.getStamp()+" 值:"+atomicStampedRef.getReference()); // false
+			}
+		});
+
+		refT1.start();
+		refT2.start();
+					
+		//System.out.println(asd.getAtomicStamp().getStamp()+"---"+asd.getAtomicStamp().getReference());
+	}
 }
