15a065ee519476128e64967cecb4fcc54fe9fab0
admin <admin@DESKTOP-565VLB9>
2018-12-12 09:47:42 +0800

    更改atomic

--- STM/src/com/dsc/Atomic/AtomicStamp.java
+++ STM/src/com/dsc/Atomic/AtomicStamp.java
@@ -3,10 +3,44 @@ package com.dsc.Atomic;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class AtomicStamp implements AtomicFactory{
+	/**
+	 * 邮标值，ref改变时改变
+	 */
+	int stampValue=0;
 	
-	int initStamp=0;
+	/**
+	 * ref
+	 */
 	Object initialRef;
+	/**
+	 * 当前类事务状态  0 初始  1  更改   2已提交
+	 */
+    int state;
+	
+    /**
+	 *  父结点  未更改前的
+	 */
 	AtomicStamp atomicStampPar;
+	
+	
+	public int getStampValue() {
+		return stampValue;
+	}
+	public void setStampValue(int stampValue) {
+		this.stampValue = stampValue;
+	}
+	public Object getInitialRef() {
+		return initialRef;
+	}
+	public void setInitialRef(Object initialRef) {
+		this.initialRef = initialRef;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
 	public AtomicStamp getAtomicStampPar() {
 		return atomicStampPar;
 	}
@@ -29,7 +63,7 @@ public class AtomicStamp implements AtomicFactory{
 	@Override
 	public void setAtomicStamp(Object initialRef) {
 		
-		atomicStamped= new AtomicStampedReference(initialRef, initStamp);
+		atomicStamped= new AtomicStampedReference(initialRef, stampValue);
 		//return this;
 	}
 	@Override
